99777c12f028e3ed3885f3431e065226c43f811b
手嶋守 <tejima@gmail.com>
2013-09-07 16:07:23 +0900

    inicial commit

---
+++ apps/pc_frontend/modules/r/actions/actions.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * r actions.
+ *
+ * @package    OpenPNE
+ * @subpackage r
+ * @author     Your name here
+ */
+class rActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfWebRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
